e06dcdc4b475924af7a22b9286ad09e2c78c8203
chenwei <robinschen1989@gmail.com>
2024-06-26 20:13:20 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "RobinsChen NextChat",
+  description: "RobinsChen personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
